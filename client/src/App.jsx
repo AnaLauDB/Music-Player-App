c02eeed650b0587cda0c1aa951491660382a7cf5
AnaLauDB <analau3217@gmail.com>
2026-07-02 13:26:28 -0600

    feat: configure application routing with React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
+import AppRouter from "./routes/AppRouter";
+
 function App() {
-  return (
-    <>
-      <h1>Music Explorer</h1>
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,11 @@
 function HomePage() {
-  return <h1>Music Explorer</h1>;
+  return (
+    <main className="container">
+      <h1>🎵 Music Explorer</h1>
+
+      <p>Tu reproductor musical con estética Pixel Art.</p>
+    </main>
+  );
 }
 
 export default HomePage;
