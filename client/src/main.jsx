94fb6882e3ea157c9cc42edf3fdd7ceaaf586290
AnaLauDB <analau3217@gmail.com>
2026-07-02 13:17:40 -0600

    feat: create global pixel art design system

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/globals.css";
+import "./styles/utilities.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
