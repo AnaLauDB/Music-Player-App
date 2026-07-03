3421c938842c0ec11200ac61cf4c02fcb5369b67
AnaLauDB <analau3217@gmail.com>
2026-07-03 12:22:20 -0600

    chore: complete frontend infrastructure setup

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
+
 function HomePage() {
+  const theme = useSelector((state) => state.app.theme);
+  console.log(import.meta.env.VITE_API_URL);
+
   return (
     <main className="container">
       <h1>🎵 Music Explorer</h1>
-
-      <p>Tu reproductor musical con estética Pixel Art.</p>
+      <p>Tema actual: {theme}</p>
     </main>
   );
 }
