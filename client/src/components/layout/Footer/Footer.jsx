c660a9c0c35613b254ffd028e73679a695a3bb89
AnaLauDB <analau3217@gmail.com>
2026-07-10 11:45:21 -0600

    feat: complete  component Footer

--- client/src/components/layout/Footer/Footer.jsx
+++ client/src/components/layout/Footer/Footer.jsx
@@ -1,5 +1,15 @@
-function Footer() {
-  return <footer>Footer</footer>;
-}
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <p>Pixel Music Player © {year}</p>
+
+      <span>Desarrollado con React + Redux + Deezer API</span>
+    </footer>
+  );
+};
 
 export default Footer;
