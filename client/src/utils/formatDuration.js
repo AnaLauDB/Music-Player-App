6ede30830252d41dbd2d636d2452480e39114761
AnaLauDB <analau3217@gmail.com>
2026-07-10 11:46:05 -0600

    feat: implement formatDuration

---
+++ client/src/utils/formatDuration.js
@@ -0,0 +1,13 @@
+const formatDuration = (seconds = 0) => {
+
+    const minutes = Math.floor(seconds / 60);
+
+    const remainingSeconds = seconds % 60;
+
+    return `${minutes}:${remainingSeconds
+        .toString()
+        .padStart(2, "0")}`;
+
+};
+
+export default formatDuration;
