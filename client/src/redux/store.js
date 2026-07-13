375c35eb9d5436e6750b3d0767dbe265b0c13359
AnaLauDB <analau3217@gmail.com>
2026-07-13 11:49:26 -0600

    feat(player): implement player redux architecture

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import appReducer from "./slices/appSlice";
 import musicReducer from "./music/musicSlice";
+import playerReducer from "./player/playerSlice";
 
 export const store = configureStore({
     reducer: {
         app: appReducer,
         music: musicReducer,
+        player: playerReducer,
     },
 });
