37383ba5e2388d70d1f6cdff8b8d2889111e584f
AnaLauDB <analau3217@gmail.com>
2026-07-13 11:48:41 -0600

    feat(player): implement player redux architecture

---
+++ client/src/redux/player/playerInitialState.js
@@ -0,0 +1,29 @@
+const playerInitialState = {
+
+    currentTrack: null,
+
+    isPlaying: false,
+
+    progress: 0,
+
+    duration: 0,
+
+    volume: 100,
+
+    muted: false,
+
+    queue: [],
+
+    currentIndex: -1,
+
+    repeat: false,
+
+    shuffle: false,
+
+    isLoading: false,
+
+    error: null,
+
+};
+
+export default playerInitialState;
