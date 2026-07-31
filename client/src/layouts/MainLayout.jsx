aedab2fb18c74a79495b5a5f1d13b68467afdd4a
AnaLauDB <analau3217@gmail.com>
2026-07-31 14:14:15 -0600

    update: favicon

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -7,7 +7,6 @@ import FeaturedPlaylists from "../components/sections/FeaturedPlaylists";
 import TopArtists from "../components/sections/TopArtists";
 import Footer from "../components/layout/Footer";
 import TrendingTracks from "../components/sections/TrendingTracks";
-// import MiniPlayer from "../components/player/MiniPlayer";
 
 function MainLayout() {
   return (
