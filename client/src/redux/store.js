5a5083ae50d5ea368ff40c9bed44fd431f3da28a
AnaLauDB <analau3217@gmail.com>
2026-09-08 17:59:48 -0600

    feat(search): add redux search foundation

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./slices/appSlice";
 import musicReducer from "./music/musicSlice";
 import playerReducer from "./player/playerSlice";
+import searchReducer from "./search/searchSlice";
 
 export const store = configureStore({
     reducer: {
         app: appReducer,
         music: musicReducer,
         player: playerReducer,
+        search: searchReducer,
     },
 });
